b1098b214fe5f2a50ee33093bd0812fe84db1799
Larky9 <remyfvy@gmail.com>
2018-03-29 08:33:08 +0200

    php

---
+++ Vue/home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Cook'isep</title>
+	<meta charset="utf8">
+</head>
+		<link href="cookcon.css" rel="stylesheet" type="text/css">
+	<body>
+	
+<div id="Header">
+	<img src="images/head.jpg" >
+</div>
+
+<div class="dessous"> 
+		
+<form action="index.php?page=login" method="post">
+		<h2>Connectez vous</h2>
+		<input type="tex" placeholder="Votre identifiant">
+		<input type = "password" value = "Mot de passe">
+		<input type="submit" name="submit">
+		<br><br>
+		<h2>ou inscrivez-vous</h2>
+		<input  type="text" placeholder="Nom">
+		<input  type="text" placeholder="Votre identifiant">
+		<br><br>
+		<input  type="text" placeholder="Votre adresse mail">
+		<br><br>
+		<input  type="text" placeholder = "Votre adresse mail">
+		<br><br>
+		<input  type="text" placeholder = "Votre mot de passe">
+		<br><br>
+		<input type="text" placeholder = "Confirmer le mot de passe">
+	    <input type="submit" name="submit">
+	</form>
+	</div>
+		</body>
+
+	<footer>
+			Vous pouvez également nous retrouver sur tous les reseaux sociaux : Facebook, Twitter, Instagram et même MySpace !
+			<br>
+			<form>
+					<input type="text">
+					<input type="submit" value="Mon avis">
+					<br>
+					<a href="https://www.facebook.com">Donnez votre avis sur notre page facebook</a>
+				</form>
+		</footer>
+		
+ </html>
---
+++ Vue/home2.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Bienvenue</title>
+<link href="home.css" rel="stylesheet" type="text/css">
+</head>
+<body><center>
+	<div id="formu">
+	<h1>Bienvenue sur Cook'isep</h1>
+	<form action="index.php?page=login" method="post">
+		<input type="text" name="login">
+		<input type="password" name="password">
+		<input type="submit" name="submit">
+	</form>
+		</div>	</center>
+</body>
+</html>
