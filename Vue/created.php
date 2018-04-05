4e7e1a06fd97607bc34ec748a897074113eae1ff
Larky9 <remyfvy@gmail.com>
2018-04-05 09:10:14 +0200

    created , existing

---
+++ Vue/created.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<p>Name: <?php echo $createdUser->getName() ?></p>
+<p>Mail: <?php echo $createdUser->getMail() ?></p>
+<body>
+</body>
+</html>
