3b815fc53fd2749946d70588e9af990395f57cf1
Mezyan Yousfi <mezyan@MacBook-Pro-de-Mezyan.local>
2018-04-05 08:55:17 +0200

    user et model

---
+++ model/model.php
@@ -0,0 +1,41 @@
+<?php
+/**
+* 
+*/
+class Model
+{
+	private $connexion;
+	
+	function __construct()
+	{
+		$servername = "localhost";
+		$username = "root";
+		$password = "root";
+		try {
+		    $conn = new PDO("mysql:host=$servername;dbname=formation_cfa", $username, $password);
+		    // set the PDO error mode to exception
+		    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+		    $this->connexion=$conn;
+		    echo "Connected successfully"; 
+	    }
+		catch(PDOException $e)
+	    {
+	    	echo "Connection failed: " . $e->getMessage();
+	    }
+	}
+	function createUser($user){
+        $this->connexion->exec("INSERT INTO `user`( `nom`, `prenom`, `mail`, `age`, `adresse`, `numero_eleve`, `login`, `password`) VALUES ('".$user->getNom()."', _
+        '".$user->getPrenom()."','".$user->getMail()."','".$user->getAge()."','".$user->getAdresse()."','".$user->getNumero_eleve()."','".$user->getLogin()."', _
+        '".$user->getPassword()."')");
+	}
+	function getUserByEmail($mail){
+		$sql =  "SELECT * FROM `user` WHERE mail='".$mail."'";
+		$user = null;
+		include_once "Model/user.php";
+	    foreach  ($this->connexion->query($sql) as $row) {
+	    	$user = new User($row['nom'],$row['mail']);
+	  	}
+	  	return $user;
+	}
+}
+?>
---
+++ model/user.php
@@ -0,0 +1,53 @@
+<?php
+
+class User
+{
+    private $nom;
+    private $mail;
+    private $prenom
+    private $id;
+    private $age
+    private $adresse
+    private $numero_eleve
+    private $login
+    private $password
+
+    function_construct($nom,$prenom,$mail,$age,$adresse,$numero_eleve,$login,$password)
+    {
+        $this->$nome=$nom;
+        $this->$mail=$mail;
+        $this->$age=$age
+        $this->prenom=$prenom
+        $this->$adresse=$adresse;
+        $this->$numero_eleve=$numero_eleve
+        $this->$login=$login
+        $this->$password=$password
+    }
+
+    function getNom(){
+        return $this->$nom;
+    }
+    function getMail(){
+        return $this->$mail;
+    }
+    function getPrenom(){
+        return $this->$prenom;
+    }
+    function getAdresse(){
+        return $this->$adresse;
+    }
+    function getAge(){
+        return $this->$age;
+    }
+    function getNumero_eleve(){
+        return $this->$numero_eleve;
+    }
+    function getLogin(){
+        return $this->$login;
+    }
+    function getPassword(){
+        return $this->$password;
+    }
+}
+
+?>
