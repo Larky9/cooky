11346e68d8cfd59a451209c0ef7bf870483ab400
mezyan <37185467+mezyan@users.noreply.github.com>
2018-04-26 18:46:34 +0200

    Add files via upload
    J'ai essayé de faire un éditeur de texte pour la page publication, mais pour une raison que j'ignore mdrrr les trois fichiers ne se lient pas entre eux et je comprends pas pourquoi
    Pourtant j'ai suivi un auto ca aurait du marcher

---
+++ Vue/publication.js
@@ -0,0 +1,14 @@
+function commande(nom, argument) {
+  if (typeof argument === 'undefined') {
+    argument = '';
+  }
+  switch (nom) {
+    case "createLink":
+      argument = prompt("Quelle est l'adresse du lien ?");
+      break;
+    case "insertImage":
+      argument = prompt("Quelle est l'adresse de l'image ?");
+      break;
+  }
+  document.execCommand(nom, false, argument);
+}
