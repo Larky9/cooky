90ff14f83714b09dcdf9629cf3a2f5296fa0d558
Rémy Févry <remyfvy@gmail.com>
2018-05-10 21:45:19 +0200

    from

--- Vue/affpost.php
+++ Vue/affpost.php
@@ -3,10 +3,17 @@
     include "model/model.php";
    
     $model = new Model();
+    $post=array();
 for ($id=1;$id<=4;$id++){
     $postes = $model->getPostes($id);
     $postes=$postes->getTitres();
-    echo "$postes <br>";
+    array_push($post,$postes);
     }
+$arrlength = count($post);
+
+for($x = 0; $x < $arrlength; $x++) {
+    echo $post[$x];
+    echo "<br>";
+}
 ?>    
       
